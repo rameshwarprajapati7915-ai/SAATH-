1e6a2dbcda408b6e19530db6dfa22c529507c766
rameshwarprajapati7915-ai <rameshwarprajapati7915@gmail.com>
2026-09-13 01:21:21 +0530

    Update Firebase authentication initialization

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMkhW31r-fGe6UTEVWwmXX5t4F4netScI",
@@ -14,7 +18,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const db = getFirestore(app);
 
 export default app;
